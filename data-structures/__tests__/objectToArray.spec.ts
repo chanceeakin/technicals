18cf074a6320280cc9453d44f9e8da103ba86de1
Chance Eakin <chance.eakin@gmail.com>
2020-12-29 20:26:30 -0600

    feat(solved): solutions to some problems

--- data-structures/__tests__/objectToArray.spec.ts
+++ data-structures/__tests__/objectToArray.spec.ts
@@ -1,8 +1,34 @@
-import {objectToArray} from '../objectToArray'
+import {arrayToObject} from '../objectToArray'
 
+const testCase = {
+  fullName: 'Alexander Hamilton',
+  age: '200',
+  gender: 'male',
+  hometown: 'NYC'
+}
 
-describe('object to array', () => {
-  it('converts an object to array', () => {
-    expect(objectToArray()).toBe(false)
+const result = [
+  ['fullName', 'Alexander Hamilton'],
+  ['age', '200'],
+  ['gender', 'male'],
+  ['hometown', 'NYC']
+]
+
+const valResult = [
+  'Alexander Hamilton',
+  '200',
+  'male',
+  'NYC'
+]
+
+// describe('object to array', () => {
+//   it('converts an object to array', () => {
+//     expect(objectToArray(testCase)).toEqual(expect.arrayContaining(result))
+//   })
+// }) 
+
+describe('array to object', () => {
+  it('converts an array to object', () => {
+    expect(arrayToObject(result)).toEqual(expect.objectContaining(testCase))
   })
 }) 
--- data-structures/objectToArray.ts
+++ data-structures/objectToArray.ts
@@ -1,3 +1,20 @@
-export function objectToArray() {
-  return 1
+// your task is to turn an object into an array
+
+
+export function objectToArray(obj: any): any {
+  return Object.entries(obj)
 }
+
+
+
+export function arrayToObject(arr: any): any {
+  const obj = {}
+  // @ts-ignore
+  arr.forEach(([key, value]: [string, string]) => {
+  // [key, value]
+  // @ts-ignore
+  obj[key] = value
+  })
+  return obj
+}
+
